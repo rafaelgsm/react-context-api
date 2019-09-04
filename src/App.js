e19480e3f4c669a430e01f656888aee9f824aae3
Rafael Montenegro <rafael@mesainc.com.br>
2019-09-04 12:30:35 -0700

    Context with functional, and refactoring BookList component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import BookList from './components/BookList';
 import ThemeContextProvider from './contexts/ThemeContext';
 import ThemeToggle from './components/ThemeToggle';
 import AuthContextProvider from './contexts/AuthContext';
+import BookContextProvider from './contexts/BookContext';
 
 
 function App() {
@@ -18,7 +19,12 @@ function App() {
 
 
           <Navbar />
-          <BookList />
+
+
+          <BookContextProvider>
+            <BookList />
+          </BookContextProvider>
+
           <ThemeToggle />
 
 
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,18 +1,26 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { BookContext } from '../contexts/BookContext';
 
 const BookList = () => {
 
     //This will provide us the context object for the THEME:
     const { isLightTheme, light, dark } = useContext(ThemeContext)
+
+    const { books } = useContext(BookContext)
+
     const theme = isLightTheme ? light : dark
 
     return (
         <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
             <ul>
-                <li style={{ background: theme.ui }}>The Way of Kings</li>
-                <li style={{ background: theme.ui }}>The Name of the Wind</li>
-                <li style={{ background: theme.ui }}>The final empire</li>
+
+                {books.map(book => {
+                    return (
+                        <li key={book.id} style={{ background: theme.ui }}>{book.title}</li>
+                    )
+                })}
+
             </ul>
         </div>
     );
